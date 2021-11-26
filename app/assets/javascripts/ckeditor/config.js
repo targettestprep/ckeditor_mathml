417f17d0f1005267ccf457f4d43ca8adb9b41cad
Amaury Peniche <apeniche@gmail.com>
2021-11-26 08:44:52 -0500

    Updates MathhaxLib path

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -23,7 +23,7 @@ CKEDITOR.editorConfig = function( config ) {
   config.specialChars = config.specialChars.concat(['&pi;', '&Pi;']);
   config.extraPlugins = 'mathjax,autogrow,link,divarea';
   config.mathJaxClass = 'math-tex-or-mml';
-  config.mathJaxLib   = window.location.protocol + '//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
+  config.mathJaxLib   = window.location.origin + '/vendor/javascript/mathjax-2.4/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
   config.extraAllowedContent = 'a table tbody tr td math mfenced merror menclose maction mmultiscripts mover mphantom mprescripts mspace mstyle msubsup mtable mtd mtr munder mun semantics mrow mfrac mn mo mtext msub mi malignmark msqrt none msup mroot maligngroup[*]{*}';
   config.indentClasses = ['indented1', 'indented2', 'indented3'];
   config.justifyClasses = [ 'text-left', 'force-center', 'text-right', 'text-justify' ] ;
