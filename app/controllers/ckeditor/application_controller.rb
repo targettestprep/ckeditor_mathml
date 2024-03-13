51793dff406323f02cb3ce00c6f6e5a60700f4de
Mukesh Patel <mukeshh92ppatel@gmail.com>
2024-03-13 18:21:05 +0530

    [RAILS-5-2-8-1] - fixes deprecated behavior render :text

--- app/controllers/ckeditor/application_controller.rb
+++ app/controllers/ckeditor/application_controller.rb
@@ -8,28 +8,28 @@ class Ckeditor::ApplicationController < ApplicationController
 
   protected
 
-    def respond_with_asset(asset)
-      file = params[:CKEditor].blank? ? params[:qqfile] : params[:upload]
-      asset.data = Ckeditor::Http.normalize_param(file, request)
+  def respond_with_asset(asset)
+    file = params[:CKEditor].blank? ? params[:qqfile] : params[:upload]
+    asset.data = Ckeditor::Http.normalize_param(file, request)
 
-      callback = ckeditor_before_create_asset(asset)
+    callback = ckeditor_before_create_asset(asset)
 
-      if callback && asset.save
-        if params[:CKEditor].blank?
-          render :json => asset.to_json(:only=>[:id, :type])
-        else
-          render :text => %Q"<script type='text/javascript'>
-              window.parent.CKEDITOR.tools.callFunction(#{params[:CKEditorFuncNum]}, '#{config.relative_url_root}#{Ckeditor::Utils.escape_single_quotes(asset.url_content)}');
-            </script>"
-        end
+    if callback && asset.save
+      if params[:CKEditor].blank?
+        render json: asset.to_json(only: [:id, :type])
       else
-        if params[:CKEditor].blank?
-          render :nothing => true, :format => :json
-        else
-          render :text => %Q"<script type='text/javascript'>
-              window.parent.CKEDITOR.tools.callFunction(#{params[:CKEditorFuncNum]}, null, '#{Ckeditor::Utils.escape_single_quotes(asset.errors.full_messages.first)}');
-            </script>"
-        end
+        render html: %Q"<script type='text/javascript'>
+            window.parent.CKEDITOR.tools.callFunction(#{params[:CKEditorFuncNum]}, '#{config.relative_url_root}#{Ckeditor::Utils.escape_single_quotes(asset.url_content)}');
+          </script>".html_safe
+      end
+    else
+      if params[:CKEditor].blank?
+        render nothing: true, format: :json
+      else
+        render html: %Q"<script type='text/javascript'>
+            window.parent.CKEDITOR.tools.callFunction(#{params[:CKEditorFuncNum]}, null, '#{Ckeditor::Utils.escape_single_quotes(asset.errors.full_messages.first)}');
+          </script>".html_safe
       end
     end
+  end
 end
